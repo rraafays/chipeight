709917c4515fb519ff1cb0fce3f26cd4aebf49b4
raf <rraf@tuta.io>
2023-06-09 23:10:50 +0100

    chore: learning how to use sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::{Sdl, VideoSubsystem};
+
 fn main() {
-    println!("i will be a chip-8 emulator written in rust");
+    init();
+    deinit();
 }
+
+fn init() {
+    let sdl: Sdl = match sdl2::init() {
+        Ok(it) => it,
+        Err(err) => panic!(),
+    };
+    let video: VideoSubsystem = match sdl.video() {
+        Ok(it) => it,
+        Err(err) => panic!(),
+    };
+
+    let window: Window = video
+        .window("test", 100, 100)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas: Canvas<Window> = window.into_canvas().present_vsync().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.present();
+}
+
+fn deinit() {}
