7b798c67914d7619176d14b0ac875c1ee8a525a0
raf <rraf@tuta.io>
2023-06-14 10:13:48 +0100

    update: major cleanup, learnt how to use the impl structure

--- src/core.rs
+++ src/core.rs
@@ -1,47 +1,11 @@
 use crate::utils;
 
+use sdl2::EventPump;
 use sdl2::{event::Event, keyboard::Keycode, render::Canvas, video::Window};
-use sdl2::{EventPump, Sdl, VideoSubsystem};
 use std::thread::sleep;
 use std::time::Duration;
 
-const WIDTH: u16 = 1024;
-const HEIGHT: u16 = 512;
-
-pub fn init() {
-    let sdl_context: Sdl = match sdl2::init() {
-        Ok(sdl_context) => sdl_context,
-        Err(_) => panic!("failed to initialise sdl context"),
-    };
-
-    let video_subsystem: VideoSubsystem = match sdl_context.video() {
-        Ok(video_subsystem) => video_subsystem,
-        Err(_) => panic!("failed to initialise video subsystem"),
-    };
-
-    let window: Window = video_subsystem
-        .window("test", WIDTH.into(), HEIGHT.into())
-        .position_centered()
-        .build()
-        .expect("failed to initialise window");
-
-    let event_pump: EventPump = match sdl_context.event_pump() {
-        Ok(event_pump) => event_pump,
-        Err(_) => panic!("failed to initialise event pump"),
-    };
-
-    let canvas: Canvas<Window> = match window.into_canvas().build() {
-        Ok(canvas) => canvas,
-        Err(_) => panic!("failed to create canvas"),
-    };
-
-    let cpu = utils::init_cpu();
-
-    // todo: load rom
-    main_loop(event_pump, canvas);
-}
-
-fn main_loop(mut event_pump: EventPump, mut canvas: Canvas<Window>) {
+pub fn main_loop(mut event_pump: EventPump, mut canvas: Canvas<Window>) {
     'running: loop {
         // todo: emulator cycle
         for event in event_pump.poll_iter() {
@@ -58,5 +22,52 @@ fn main_loop(mut event_pump: EventPump, mut canvas: Canvas<Window>) {
         // todo: build texture
         canvas.present();
         sleep(Duration::new(0, 16 * 1000 * 1000));
+    }
+}
+
+pub struct CPU {
+    pub opcode: u16,
+    pub memory: [u8; 4096],
+    pub graphics: [u8; 64 * 32],
+
+    pub register: [u8; 16],
+    pub index_register: u16,
+    pub program_counter: u16,
+
+    pub delay_timer: u8,
+    pub sound_timer: u8,
+
+    pub stack: [u16; 16],
+    pub stack_pointer: u16,
+
+    pub keys: [u8; 16],
+}
+
+impl CPU {
+    pub fn init() -> CPU {
+        let mut cpu = CPU {
+            opcode: 0,
+            memory: [0; 4096],
+            graphics: [0; 2048],
+
+            register: [0; 16],
+            index_register: 0,
+            program_counter: 0x200,
+
+            delay_timer: 0,
+            sound_timer: 0,
+
+            stack: [0; 16],
+            stack_pointer: 0,
+
+            keys: [0; 16],
+        };
+
+        for (index, byte) in utils::FONTSET.iter().enumerate() {
+            cpu.memory[index] = *byte;
+            println!("{index} = {byte}");
+        }
+
+        return cpu;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
 mod core;
 mod utils;
 
+use sdl2::{render::Canvas, video::Window};
+use sdl2::{EventPump, Sdl, VideoSubsystem};
+
+const WIDTH: u16 = 1024;
+const HEIGHT: u16 = 512;
+
 fn main() {
-    core::init();
+    init();
+}
+
+pub fn init() {
+    let sdl_context: Sdl = match sdl2::init() {
+        Ok(sdl_context) => sdl_context,
+        Err(_) => panic!("failed to initialise sdl context"),
+    };
+
+    let video_subsystem: VideoSubsystem = match sdl_context.video() {
+        Ok(video_subsystem) => video_subsystem,
+        Err(_) => panic!("failed to initialise video subsystem"),
+    };
+
+    let window: Window = video_subsystem
+        .window("test", WIDTH.into(), HEIGHT.into())
+        .position_centered()
+        .build()
+        .expect("failed to initialise window");
+
+    let event_pump: EventPump = match sdl_context.event_pump() {
+        Ok(event_pump) => event_pump,
+        Err(_) => panic!("failed to initialise event pump"),
+    };
+
+    let canvas: Canvas<Window> = match window.into_canvas().build() {
+        Ok(canvas) => canvas,
+        Err(_) => panic!("failed to create canvas"),
+    };
+
+    let mut cpu = core::CPU::init();
+
+    // todo: load rom
+    core::main_loop(event_pump, canvas);
 }
