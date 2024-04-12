685cfe4b222fef3f7c2b98378b2f61aa9e3139fc
raf <rraf@tuta.io>
2024-04-12 04:44:18 +0100

    chore: nix changes

--- shell.nix
+++ shell.nix
@@ -2,9 +2,11 @@
 pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     cargo
+  ];
+  buildInputs = with pkgs; [
     rustc
     rust-analyzer
     rustfmt
     SDL2
-  ];
+  ] ++ lib.optional stdenv.isDarwin libiconv;
 }
