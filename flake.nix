26ee03626bcef5dbc1e027e0e7340e43a8b0a3ee
raf <rraf@tuta.io>
2024-09-09 10:20:15 +0100

    chore: pinned flake version

--- flake.nix
+++ flake.nix
@@ -1,25 +1,35 @@
 {
-  description = "Build shell with any rust dependencies";
+  description = "Build Shell with any dependency of the project";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOs/nixpkgs";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs =
+            with pkgs;
+            [
               cargo
               rustc
               rustfmt
               rust-analyzer
               SDL2
-            ] ++ lib.optional stdenv.isDarwin libiconv;
-          };
-        }
-      );
+            ]
+            ++ lib.optional stdenv.isDarwin libiconv;
+        };
+      }
+    );
 }
